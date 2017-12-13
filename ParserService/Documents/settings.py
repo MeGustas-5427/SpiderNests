65f2a09caba78df4ec38d19de1687b9263e53997
子休 <1191170766@qq.com>
2017-12-13 20:20:36 +0800

    update

---
+++ ParserService/Documents/settings.py
@@ -0,0 +1,27 @@
+"""
+settings for ParserService
+"""
+import os
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+#
+LOGS = {
+
+}
+
+#
+TASKS = {
+
+}
+
+#
+RESULTS = {
+
+}
+
+#
+MONITORS = {
+
+}
