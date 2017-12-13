590e7239eabd36eca6506d57d0884b24dfaa8fd5
子休 <1191170766@qq.com>
2017-12-13 17:52:05 +0800

    Update

---
+++ ParserService/ParserDir/main.py
@@ -0,0 +1,16 @@
+"""
+A example for writing parser-file.
+
+The Class name must be "HtmlParser".
+The function "run" is necessary.
+If the class "HtmlParser" don't have it, it will throw a error.
+"""
+class HtmlParser:
+    def __init__(self):
+        pass
+
+    def run(self):
+        result = dict
+        # other sentences for parse pages and clear data
+        # return a dictionary that the datas in the end
+        return result
